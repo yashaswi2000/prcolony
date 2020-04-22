7f20fa0bdad8436b610067acfeb36ef3f1a66149
yashaswi2000 <2017csb1089@iitrpr.ac.in>
2020-04-22 14:15:21 +0530

    depend

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:prcolony/screens/Authenticate/signup.dart';
+import 'package:prcolony/screens/wrapper.dart';
 
+void main() => runApp(MyApp());
 
-void main() => runApp(MaterialApp(
-  home: SignUp(),
-));
-
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Wrapper(),
+    );
+  }
+  
+  
+}
