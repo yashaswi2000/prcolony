72b792b4c94c7db0e3446e19a5ae5eed27c919ec
yashaswi2000 <2017csb1089@iitrpr.ac.in>
2020-04-22 16:10:30 +0530

    wrapper done

--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -1,9 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:prcolony/screens/Authenticate/authenticate.dart';
+import 'package:prcolony/screens/Authenticate/signup.dart';
 import 'package:prcolony/screens/Home/home.dart';
+import 'package:provider/provider.dart';
+import 'package:prcolony/models/User.dart';
 
 class Wrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Home();
+    final user = Provider.of<User>(context);
+    print(user);
+
+    if (user == null)
+    {
+        return SignUp();
+    }
+    else{
+      return Home();
+    }
+    
   }
 }
