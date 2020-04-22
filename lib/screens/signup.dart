83c4730583b05339e8e1e67ab4d73f15e84a55f9
yashaswi2000 <2017csb1089@iitrpr.ac.in>
2020-04-22 11:30:36 +0530

    ordering

--- lib/screens/signup.dart
+++ lib/screens/signup.dart
@@ -1,59 +1,37 @@
-
 import 'package:flutter/material.dart';
+
+
 class SignUp extends StatelessWidget {
   @override
-
   Widget build(BuildContext context) {
     return MaterialApp(
-     home : Scaffold(
-       body : Container(
-         child : Column(
-           children: <Widget>[
-      
-      TextField(
-  decoration: InputDecoration(
-    border: InputBorder.none,
-    labelText: 'Enter your name:'
-  ),
-),
-TextField(
-  decoration: InputDecoration(
-    border: InputBorder.none,
-    labelText: 'mobile number:'
-  ),
-   keyboardType: TextInputType.number,
-            
-),
-
-TextField(
-  decoration: InputDecoration(
-    border: InputBorder.none,
-  labelText: 'street no:'
-  ),
-   keyboardType: TextInputType.number,
-            
-),
-TextField(
-  decoration: InputDecoration(
-    border: ,
-  labelText: 'plot no:'
-  ),
-   keyboardType: TextInputType.number,
-            
-),
-TextField(
-  decoration: InputDecoration(
-    border: InputBorder.none,
-  labelText: 'full address:'
-  ),
-),
-RaisedButton(
-            child : Text("submit"),onPressed:null   
-            ),
-           ]
-        )
-           
-       )
-    ),);
+      home: Scaffold(
+          body: Container(
+              child: Column(children: <Widget>[
+        TextField(
+          decoration: InputDecoration(
+              border: InputBorder.none, labelText: 'Enter your name:'),
+        ),
+        TextField(
+          decoration: InputDecoration(
+              border: InputBorder.none, labelText: 'mobile number:'),
+          keyboardType: TextInputType.number,
+        ),
+        TextField(
+          decoration: InputDecoration(
+              border: InputBorder.none, labelText: 'street no:'),
+          keyboardType: TextInputType.number,
+        ),
+        TextField(
+          decoration: InputDecoration(labelText: 'plot no:'),
+          keyboardType: TextInputType.number,
+        ),
+        TextField(
+          decoration: InputDecoration(
+              border: InputBorder.none, labelText: 'full address:'),
+        ),
+        RaisedButton(child: Text("submit"), onPressed: null),
+      ]))),
+    );
   }
 }
